258029f393249861de8749a23385975df70d3567
Ankit Sahay <ankit.sahay@rysun.com>
2024-10-24 10:55:28 +0530

    create cypress test 001

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+describe('sauce demo spec', () => {
+  it('logs in', () => {
+    cy.visit('https://saucedemo.com');
+    cy.get("[data-test='username']").type("standard_user");
+    cy.get("[data-test='password']").type("secret_sauce");
+    cy.get("[data-test='login-button']").click();
+    cy.get('.title').should('have.text','Products');  
+  })
+})
