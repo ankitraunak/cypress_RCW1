51f2de36ec1ca7dc0165a60d6445deb186d2abab
ankit raunak sahay <ankitraunak.sahay@gmail.com>
2024-10-24 09:11:06 +0000

    Update file spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,7 @@ describe('sauce demo spec', () => {
   it('logs in', () => {
     cy.visit('https://saucedemo.com');
     cy.get("[data-test='username']").type("standard_user");
-    cy.get("[data-test='password']").type("secret_sauce");
+    cy.get("[data-test='password']").type("secret_saucey");
     cy.get("[data-test='login-button']").click();
     cy.get('.title').should('have.text','Products');  
   })
