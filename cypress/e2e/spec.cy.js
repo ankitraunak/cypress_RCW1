491e8f5b510a2df8644b0f452a53be601339eb82
ankit raunak sahay <ankitraunak.sahay@gmail.com>
2024-11-06 06:12:57 +0000

    Update file spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,16 @@
 describe('sauce demo spec', () => {
   it('logs in', () => {
-    cy.visit('https://saucedemo.com');
-    cy.get("[data-test='username']").type("standard_user");
-    cy.get("[data-test='password']").type("secret_sauce");
-    cy.get("[data-test='login-button']").click();
-    cy.get('.title').should('have.text','Product');  
+    cy.visit('https://rcwtest.rcwilley.com/');
+    cy.wait(3000)
+    cy.get('body').then($body => {
+        if ($body.find('.modal-header').length === 1) {
+            cy.get('.btn-close').click();
+            cy.log("modal header appeared");
+        }
+      })
+    cy.wait(3000);
+        cy.get('.tile-container').should('be.visible')
+        cy.get('.tile-container').find('.tag-tile').contains("Refrigerators").click();
+        cy.url().should('include', text);  
   })
 })
